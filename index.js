1037c88be6baef25e2eb673cc9d690ba232295a0
sawaneh1 <sawaneh.itg.@gmail.com>
2022-03-02 23:01:58 +0000

    first commit

--- index.js
+++ index.js
@@ -28,26 +28,6 @@ mongoose
   )
   .catch((error) => console.log(`${error} did not connect`));
 
-// app.use(async (req, res, next) => {
-//   if (req.headers["x-access-token"]) {
-//     const accessToken = req.headers["x-access-token"];
-//     const { userId, exp } = await jwt.verify(
-//       accessToken,
-//       process.env.JWT_SECRET
-//     );
-//     // Check if token has expired
-//     if (exp < Date.now().valueOf() / 1000) {
-//       return res.status(401).json({
-//         error: "JWT token has expired, please login to obtain a new one",
-//       });
-//     }
-//     res.locals.loggedInUser = await User.findById(userId);
-//     next();
-//   } else {
-//     next();
-//   }
-// });
-
 app.use("/", customerRoute);
 app.use("/", businessRoute);
 app.use("/", riderRoute);
